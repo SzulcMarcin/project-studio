f2ed754c6dd7d57747fcb0ce4b763248e5367abf
Marcin Szulc <marcin.szulc@o2.pl>
2020-10-11 12:20:04 +0200

    add section header with hero-img

--- script.js
+++ script.js
@@ -7,14 +7,14 @@ $('document').ready(function () {
     });
 
     function burgerTime() {
-      if (isClosed == true) {
+      if (isClosed == false) {
         trigger.removeClass('is-open');
         trigger.addClass('is-closed');
-        isClosed = false;
+        isClosed = true;
       } else {
         trigger.removeClass('is-closed');
         trigger.addClass('is-open');
-        isClosed = true;
+        isClosed = false;
       }
     }
 
