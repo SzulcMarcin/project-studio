aee48793a741d0452e5a5cc1295f5f1842f6bf2d
Marcin Szulc <marcin.szulc@o2.pl>
2020-10-12 16:15:07 +0200

    add function js-navbar-fix

--- script.js
+++ script.js
@@ -21,3 +21,13 @@ $('document').ready(function () {
     }
 
   });
+// navbar-fix
+  $(document).ready(function () {
+    $(document).click(function (event) {
+        var clickover = $(event.target);
+        var _opened = $(".navbar-collapse").hasClass("show");
+        if (_opened === true && !clickover.hasClass("navbar-toggler")) {
+            $(".navbar-toggler").click();
+        }
+    });
+});
